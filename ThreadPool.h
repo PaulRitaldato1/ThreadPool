f89318cff2d3a90c3cd6515efa0ca0c8f8fafe21
Paul Ritaldato <42816832+PaulRitaldato1@users.noreply.github.com>
2020-06-04 21:44:15 -0400

    Update ThreadPool.h

--- ThreadPool.h
+++ ThreadPool.h
@@ -14,27 +14,6 @@
 #include <string_view>
 #include "../../Global/Macros.h"
 
-template <class T>
-constexpr
-std::string_view
-type_name()
-{
-	std::string_view name, prefix, suffix;
-	#if defined(__GNUC__)
-    name = __PRETTY_FUNCTION__;
-    prefix = "constexpr std::string_view type_name() [with T = ";
-    suffix = "; std::string_view = std::basic_string_view<char>]";
-	#elif defined(_MSC_VER)
-    name = __FUNCSIG__;
-    prefix = "class std::basic_string_view<char,struct std::char_traits<char> > __cdecl type_name<";
-    suffix = ">(void)";
-	#endif
-
-	name.remove_prefix(prefix.size());
-    name.remove_suffix(suffix.size());
-    return name;
-}
-
 /* ThreadPool class */
 class ThreadPool
 {
@@ -84,7 +63,7 @@ public:
 			jobQueue.emplace(std::packaged_task<void()>(std::move(task)));
 			//jobQueue.emplace(std::make_shared<AnyJob<retType> >(std::move(task)));
 		}
-        notifier.notify_one();
+        	notifier.notify_one();
 		return future;
     }
 
