b93a070373290bd579b0d175cdd4513ff3146d2c
Paul Ritaldato <42816832+PaulRitaldato1@users.noreply.github.com>
2020-08-31 18:25:13 -0400

    Update ThreadPool.h

--- ThreadPool.h
+++ ThreadPool.h
@@ -10,42 +10,35 @@
 #include <atomic>
 #include <type_traits>
 #include <typeinfo>
-#include <array>
 
 /* ThreadPool class */
-template <std::size_t PoolSize = 1>
 class ThreadPool
 {
 public:
-	/*  Constructors */
-	//DEPRECATE TAG: Changed create threads to use compile time template param
-	//ThreadPool(std::size_t numThreads)
-	//END TAG
+
 	ThreadPool()
 	{
-		//DEPRECATE TAG: Changed create threads to use compile time template param
-		//createThreads(numThreads);
-		//END TAG
-		createThreads();
+		m_shutdown.store(false, std::memory_order_relaxed);
+		//m_shutdown = false;
+		createThreads(1);
 	}
 
-	//DEPRECATE TAG: Removing this default constructor in favor of compile time 
-		//ThreadPool()
-		//{
-		//	const size_t numThreads = (size_t)std::thread::hardware_concurrency();
-		//	createThreads(numThreads);
-		//}
-	/* end constructors */
-	//END TAG
+	ThreadPool(std::size_t numThreads)
+	{
+		m_shutdown.store(false, std::memory_order_relaxed);
+		//m_shutdown = false;
+		createThreads(numThreads);
+	}
 
-	//Destructor
 	~ThreadPool()
 	{
-		killThreads = true;
-		notifier.notify_all();
-		for (std::size_t i = 0; i < PoolSize; ++i)
+		m_shutdown.store(true, std::memory_order_relaxed);
+		//m_shutdown = true;
+		m_notifier.notify_all();
+
+		for (std::thread& th : m_threads)
 		{
-			threads[i].join();
+			th.join();
 		}
 	}
 
@@ -53,75 +46,66 @@ public:
 	template <typename Func, typename... Args>
 	auto enqueue(Func&& f, Args&&... args)
 	{
-
-		//typedef decltype(f(args...)) RetType;
-
 		//get return type of the function
 		using RetType = std::invoke_result_t<Func, Args...>;
 
-		auto task = std::make_shared<std::packaged_task<RetType()>>(std::bind(f, std::forward<Args>(args)...));
-		
+		auto task = std::make_shared<std::packaged_task<RetType()>>([&f, &args...]() { return f(std::forward<Args>(args)...); });
+
 		{
 			// lock jobQueue mutex, add job to the job queue 
-			std::unique_lock<std::mutex> lock(JobMutex);
+			std::scoped_lock<std::mutex> lock(m_jobMutex);
 
 			//place the job into the queue
-			jobQueue.emplace([=] {
+			m_jobQueue.emplace([task]() {
 				(*task)();
 			});
 		}
-		notifier.notify_one();
+		m_notifier.notify_one();
 
 		return task->get_future();
 	}
 	
-	/* utility functions will go here*/
-	constexpr std::size_t getThreadCount() const {
-		return threads.size();
+	/* utility functions */
+	std::size_t getThreadCount() const {
+		return m_threads.size();
 	}
 
 private:
 
 	using Job = std::function<void()>;
-	std::array<std::thread, PoolSize> threads;
-	std::queue<Job> jobQueue;
-	std::condition_variable notifier;
-	std::mutex JobMutex;
-	std::atomic<bool> killThreads = false;
+	std::vector<std::thread> m_threads;
+	std::queue<Job> m_jobQueue;
+	std::condition_variable m_notifier;
+	std::mutex m_jobMutex;
+	std::atomic<bool> m_shutdown;
 
-	void createThreads()
+	void createThreads(std::size_t numThreads)
 	{
-		constexpr std::size_t numThreads = PoolSize;
+		m_threads.reserve(numThreads);
 		for (int i = 0; i != numThreads; ++i)
 		{
-			threads[i] = std::thread([this]()
+			m_threads.emplace_back(std::thread([this]()
 			{
 				while (true)
 				{
 					Job job;
 
 					{
-						std::unique_lock<std::mutex> lock(JobMutex);
-						notifier.wait(lock, [this] {return !jobQueue.empty() || killThreads; });
+						std::unique_lock<std::mutex> lock(m_jobMutex);
+						m_notifier.wait(lock, [this] {return !m_jobQueue.empty() || m_shutdown.load(std::memory_order_relaxed); });
 
-						if (killThreads)
+						if (m_shutdown.load(std::memory_order_relaxed))
 						{
 							break;
 						}
 
-						// //strange bug where it will continue even if the job queue is empty
-						// if (jobQueue.size() < 1)
-						// 	continue;
+						job = std::move(m_jobQueue.front());
 
-						job = std::move(jobQueue.front());
-
-						jobQueue.pop();
+						m_jobQueue.pop();
 					}
 					job();
 				}
-			});
+			}));
 		}
 	}
-	ThreadPool (const ThreadPool&) = delete;
-	ThreadPool& operator= (const ThreadPool&) = delete;
 }; /* end ThreadPool Class */
